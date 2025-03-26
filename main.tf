bc5ed7d2a41f6ed2edca3f7dfc50fd1e23be3fed
arundaky <arun.d2k@gmail.com>
2025-03-26 12:34:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_launch_template" "web_server_as" {
     image_id           = "ami-04aa00acb1165b32a"
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t2.micro"
-    key_name = "lastone"
+    key_name = "app-key"
     tags = {
         Name = "DevOps"
     }

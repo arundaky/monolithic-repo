4fc1367746c338d8f4c49c3cc2eaf6c667844887
arundaky <arun.d2k@gmail.com>
2025-05-29 11:39:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_launch_template" "web_server_as" {
     name = "myproject"
-    image_id           = "ami-04aa00acb1165b32a"
+    image_id           = "ami-0e58b56aa4d64231b"
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t2.micro"
     key_name = "app-key"
